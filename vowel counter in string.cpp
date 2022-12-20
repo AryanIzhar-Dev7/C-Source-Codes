932fd3578b1106b679f6aa8e219223206f01d95b
Aryan Izhar <115881821+AryanIzhar-Dev7@users.noreply.github.com>
2022-12-20 20:50:45 +0500

    Vowel counter:
    This program will count the vowel letters in the given word input

---
+++ vowel counter in string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+string word;
+cout<<"Enter the word.. ";
+	cin>>word;
+	
+
+	int count=0;
+	for(int i=0;i< word.length(); i++){
+		
+		if (word[i]=='a'||word[i]=='e'|| word[i]=='i'||word[i]=='i'||word[i]=='o'||word[i]=='u'){
+		count++;	
+		}
+	}
+	
+	cout<<"There are "<< count<<" Vowels in the word .. ";
+
+
+return 0;
+}
+
